9418cbcd998baadb1411585abbc91fcbfe8b9ff5
bwax <braxton.waxdeck@gmail.com>
2023-07-18 20:24:01 -0700

    update index layout

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,21 @@ import ReviewCarousel from "@/components/ReviewCarousel";
 export default function Home() {
   return (
     <>
-    <IconList list={destinations}/>
+
       <div className="font-sans container mx-auto p-4">
         <h1 className="text-3xl font-semibold flex justify-center">Welcome</h1>
         <p className=' text-lg p-2 sm:p-4 flex justify-center'>
-          to Alma Vista, the gateway to exploring the Sierra Nevadas. A place to connect with nature, people, and the moment. 
+          to Alma Vista, the gateway to exploring the Sierra Nevadas. A place to connect with nature, people, and the moment.
         </p>
       </div>
       <div className="py-6">
-        <ReviewCarousel/>
+        <ReviewCarousel />
+      </div>
+      <div className="p-4 pt-8">
+        <IconList list={destinations}/>
       </div>
       <div className='p-4 sm:p-12'>
-        <Guidebook/>
+        <Guidebook />
       </div>
     </>
   );
