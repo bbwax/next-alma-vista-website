4fc81775858535a452e7ab1ca00b9caaf1bf0f60
bwax <braxton.waxdeck@gmail.com>
2023-07-21 10:18:52 -0700

    test 4 preview update

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <title> Alma Vista | Luxury Vacation Cabin in Sierra Nevadas | Home Theater | Hot Tub | EV Charger</title>
+                <title> Alma Vista - Luxury Vacation Cabin in Sierra Nevadas</title>
                 <meta name="description" content="Welcome to Alma Vista. Experience the comfort of our luxury vacation rental home in the heart of the Sierra Nevadas. Perfectly located near Yosemite and Pinecrest, our home offers the ideal base for your outdoor adventures or serene getaway." />
                 <meta name="keywords" content="Vacation Rental, Sierra Nevadas, Yosemite, Pinecrest, Luxury Home, California Vacation Home, Outdoor Adventure, Hiking, Nature, Getaway" />
                 <meta property="og:title" content="Alma Vista | Vacation Cabin Near Yosemite & Pinecrest | Home Theater | Hot Tub | EV Charger " />
-                <meta property="og:description" content="Experience the comfort of our luxury vacation rental home in the heart of the Sierra Nevadas. Perfectly located near Yosemite and Pinecrest, our home offers the ideal base for your outdoor adventures or serene getaway." />
+                <meta property="og:description" content="Experience the comfort of Alma Vista, a luxury vacation rental home in the heart of the Sierra Nevadas. Perfectly located near Yosemite and Pinecrest, our home offers the ideal base for your outdoor adventures or serene getaway." />
                 <meta property="og:image" content="/images/AlmaVistaSunset.jpg" />
                 <meta property="og:url" content="https://www.almavistacabin.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat" rel="stylesheet" />
