677cc13286d7d15fa3315dcaaf6cae5210662267
bwax <braxton.waxdeck@gmail.com>
2023-07-21 14:28:22 -0700

    stopped scrolling for navbar icon

--- components/NavbarIcon.js
+++ components/NavbarIcon.js
@@ -23,6 +23,18 @@ export default function NavbarIcon() {
             fetchQuote();
         }
     }, [isOpen]);
+    
+    useEffect(() => {
+        if (isOpen) {
+          document.body.style.overflow = 'hidden';
+        } else {
+          document.body.style.overflow = 'unset';
+        }
+    
+        return () => {
+          document.body.style.overflow = 'unset';
+        };
+      }, [isOpen]);
 
     return (
         <>
